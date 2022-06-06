3375d32fb5adb9955b7e5d0d02caa63ff3df64ad
Lucas Bäckman <lucas.backman@gmail.com>
2022-06-06 13:05:07 +0300

    Add driver script

--- lib/main.rb
+++ lib/main.rb
@@ -6,5 +6,33 @@ require_relative 'node'
 arr = Array.new(15) { rand(1..100) }
 
 tr = Tree.new(arr)
+tr.pretty_print
+puts "Tree is balanced? : #{tr.balanced?}"
+puts "In-order: #{tr.inorder}"
+puts "Pre-order: #{tr.preorder}"
+puts "Post-order: #{tr.postorder}"
+puts "Level-order: #{tr.level_order}"
+
+tr.insert(190)
+tr.insert(299)
+tr.insert(101)
+tr.insert(234)
+tr.insert(1000)
+tr.insert(-1)
+tr.insert(-2)
 
 tr.pretty_print
+puts "Tree is balanced? : #{tr.balanced?}"
+puts "In-order: #{tr.inorder}"
+puts "Pre-order: #{tr.preorder}"
+puts "Post-order: #{tr.postorder}"
+puts "Level-order: #{tr.level_order}"
+
+tr.rebalance
+
+tr.pretty_print
+puts "Tree is balanced? : #{tr.balanced?}"
+puts "In-order: #{tr.inorder}"
+puts "Pre-order: #{tr.preorder}"
+puts "Post-order: #{tr.postorder}"
+puts "Level-order: #{tr.level_order}"
